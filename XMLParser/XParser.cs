2a7c742129d93534fd418c5f8dfed049e5e62d52
Iost Aleksey <iostaleksey@gmail.com>
2019-07-18 14:38:31 +0700

    Update XParser.cs

--- XMLParser/XParser.cs
+++ XMLParser/XParser.cs
@@ -12,28 +12,6 @@ namespace XMLParser
     public static class XParser
     {
         /// <summary>
-        /// Возвращет nullable значение для элемента
-        /// </summary>
-        /// <typeparam name="T">Тип возвращаемого значения</typeparam>
-        /// <param name="element">Элемент, значение которого хотим получить</param>
-        /// <returns>Возвращает либо значение элемента, либо null</returns>
-        public static T? GetXElementValueNullable<T>(XElement element) where T : struct
-        {
-            return element == null ? null : (T?)Convert.ChangeType(element.Value, typeof(T), CultureInfo.InvariantCulture);
-        }
-        /// <summary>
-        /// Возвращет nullable значение для элемента
-        /// </summary>
-        /// <typeparam name="T">Тип возвращаемого значения</typeparam>
-        /// <param name="element">Элемент, внутри которого будет происходить поиск тегов</param>
-        /// <param name="tags">Список тегов без namespace, по которым надо пройти для достижения искомого тега с искомым значением. Последний элемент последовательности - искомый тег</param>
-        /// <returns>Возвращает либо значение элемента, либо null</returns>
-        public static T? GetXElementValueNullable<T>(XElement element, params string[] tags) where T : struct
-        {
-            var res = GetXElementList(element, tags).FirstOrDefault();
-            return res == null ? null : (T?)Convert.ChangeType(res.Value, typeof(T), CultureInfo.InvariantCulture);
-        }
-        /// <summary>
         /// Возвращет значение для элемента
         /// </summary>
         /// <typeparam name="T">Тип возвращаемого значения</typeparam>
@@ -41,7 +19,8 @@ namespace XMLParser
         /// <returns>Возвращает либо значение элемента, либо дефолтное значение типа</returns>
         public static T GetXElementValue<T>(XElement element)
         {
-            return element == null ? default(T) : (T)Convert.ChangeType(element.Value, typeof(T), CultureInfo.InvariantCulture);
+            var type = Nullable.GetUnderlyingType(typeof(T)) ?? typeof(T);
+            return element == null ? default(T) : (T)Convert.ChangeType(element.Value, type, CultureInfo.InvariantCulture);
         }
         /// <summary>
         /// Возвращет значение для элемента
@@ -53,7 +32,8 @@ namespace XMLParser
         public static T GetXElementValue<T>(XElement element, params string[] tags)
         {
             var res = GetXElementList(element, tags).FirstOrDefault();
-            return res == null ? default(T) : (T)Convert.ChangeType(res.Value, typeof(T), CultureInfo.InvariantCulture);
+            var type = Nullable.GetUnderlyingType(typeof(T)) ?? typeof(T);
+            return res == null ? default(T) : (T)Convert.ChangeType(res.Value, type, CultureInfo.InvariantCulture);
         }
         /// <summary>
         /// Возвращает список искомых тегов
